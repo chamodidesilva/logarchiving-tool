25c2b41ed0a1681a9785fc05acfa7396bf0af911
Chamodi De Silva <chamodidesil@gmail.com>
2025-04-26 16:20:02 +0530

    script for logarchive tool installation

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+#Author: Chamodi De Silva
+#Date: 2025-04-26
+#Description: This is the setup script for the logarchivetool.sh script. Run the script with superuser privileges.
+#Usage: ./setup.sh
+
+chmod +x logarchivetool.sh
+mv logarchivetool.sh /usr/local/bin/logarchive
