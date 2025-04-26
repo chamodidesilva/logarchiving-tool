b4e7fb8fe2fefaedacf4cfcc832c4b2b192759d2
Chamodi De Silva <chamodidesil@gmail.com>
2025-04-26 14:10:00 +0530

    add error handling and usage

--- logarchivetool.sh
+++ logarchivetool.sh
@@ -1,12 +1,34 @@
 #!/bin/bash
 
-#This is a command line tool for log archiving
+#Author: Chamodi De Silva
+#Date: 2025-04-26
+#Version: 1.0
+#Description: This script will take a log file as an argument and create an archive of the log file and create a new directory to store the archive
+#Usage: ./logarchivetool.sh <logfile>
+#Example: ./logarchivetool.sh /var/log/syslog
+
+usage() {
+    echo "Usage: $0 <logfile>"
+    echo "Example: $0 /var/log/syslog"
+}
+
+if [ "$#" -eq 0 ]; then
+    echo "Error: No arguments provided"
+    usage
+    exit 1
+elif [ "$#" -gt 1 ]; then
+    echo "Error: Too many arguments"
+    usage
+    exit 1
+elif [ ! -e "$1" ]; then
+    echo "Error: File not found"
+    usage
+    exit 1
+fi
 
 logfile_name=$(basename "$1")
-#echo "$logfile_name"
 tarball_name="${logfile_name}_$(date +"%Y%m%d")_$(date +"%H%M%S").tar.gz"
-#echo "$tarball_name"
 newdir_name="newarchive"
 
-sudo mkdir "$newdir_name"
-sudo tar -czf "$newdir_name/$tarball_name" "$1"
+mkdir -p "$newdir_name"
+tar -czf "$newdir_name/$tarball_name" "$1" > /dev/null 2>&1
